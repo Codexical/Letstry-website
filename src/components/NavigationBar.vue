aa69be4d2afb529b61d06c213a78ddff92e8a663
AU2A <aura.cs12@nycu.edu.tw>
2024-05-21 11:54:46 +0800

    navbar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app-bar>
+        <v-toolbar-title>Hi!</v-toolbar-title>
+        <v-menu>
+            <template v-slot:activator="{ props }">
+                <v-btn color="primary" v-bind="props">
+                    常態課程
+                </v-btn>
+            </template>
+            <v-list>
+                <v-list-item v-for="(item, index) in items" :key="index" @click="course(item.url)">
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        items: [
+        ],
+    }),
+}
+</script>
