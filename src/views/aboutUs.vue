cdbdfb69b1937a8f8653b68be8125215db1b4414
Aura <aura.cs12@nycu.edu.tw>
2024-06-17 10:36:21 +0800

    add about page

---
+++ src/views/aboutUs.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-img src="../assets/logo.png" max-height="300"></v-img>
+    <div class="d-flex align-center justify-center flex-wrap text-center mx-auto px-4" elevation="4" max-width="800"
+        width="100%" rounded>
+        <div>
+            <h2 class="text-h4 font-weight-black text-orange mt-10 mb-3">樂創機器人</h2>
+
+            <div class="text-h5 font-weight-medium mb-2">
+                營業時間
+            </div>
+
+            <div class="text-body-3 mb-10">
+                <p>週一公休</p>
+                <p>週二至週五 14:00-21:00</p>
+                <p>週六至週日 09:00-18:00</p>
+                <p>國定假日營業時間請見FB專頁公告</p>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex align-center justify-center flex-wrap text-center mx-auto px-4">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3682.836739378743!2d120.31686327543093!3d22.622570979456007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e049819516491%3A0x1545af28e9197145!2z5qiC5Ym15qmf5Zmo5Lq6!5e0!3m2!1szh-TW!2stw!4v1718590129180!5m2!1szh-TW!2stw"
+            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+    </div>
+</template>
