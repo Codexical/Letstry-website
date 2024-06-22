69ee591ee09924f05e302d128f1fc386d54a3f66
Aura <aura.cs12@nycu.edu.tw>
2024-06-22 16:43:07 +0800

    add bottom bar

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,5 +1,26 @@
 <template>
-    <div class="grey-lighten-3 text-center mt-10 mb-10">
-        <span>@LetsTryRobot. All rights reserved.</span>
+    <div class="bg-grey-lighten-3 d-flex justify-center flex-wrap pt-3 pb-3">
+        <div class="pa-2 ma-2">
+            <v-btn color="indigo-darken-3" @click="facebook()">Facebook: 樂創機器人</v-btn>
+        </div>
+        <div class="pa-2 ma-2">
+            <v-btn color="indigo-darken-3" @click="tel()">連絡電話: (07)7219590</v-btn>
+        </div>
+    </div>
+    <div class="bg-grey-darken-2 text-center">
+        <span>Copyright © 2024 Letstry.Robot. All rights reserved.</span>
     </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        facebook() {
+            window.open('https://www.facebook.com/Letstry.Robot/', '_blank');
+        },
+        tel() {
+            window.open('tel:07-7219590', '_blank');
+        }
+    }
+}
+</script>
